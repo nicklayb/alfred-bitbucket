373180807470c0b85954339ae2dc30d0dd8776ab
Jonas Reitmann <jonas@reitmann.me>
2019-07-20 17:54:11 +0200

    Re-indent using spaces

--- bitbucket/pipelineService.js
+++ bitbucket/pipelineService.js
@@ -2,31 +2,31 @@ const createService = require('../utils').createService;
 const moment = require('moment');
 
 const url = (host) => {
-	return host + ['repositories', process.env.repo, 'pipelines/'].join('/');
+    return host + ['repositories', process.env.repo, 'pipelines/'].join('/');
 };
 
 const map = ({ build_number, created_on, repository, creator, state }) => {
-	const getTitle = () => {
-		if (state.result && state.result.name === 'FAILED') {
-			return '❌'
-		}
+    const getTitle = () => {
+        if (state.result && state.result.name === 'FAILED') {
+            return '❌'
+        }
 
-		return '✅'
-	};
+        return '✅'
+    };
 
-	const getSubtitle = () => {
-		const createdOn = moment(created_on).fromNow();
+    const getSubtitle = () => {
+        const createdOn = moment(created_on).fromNow();
 
-		return `${creator.display_name} - ${createdOn}`
-	};
+        return `${creator.display_name} - ${createdOn}`
+    };
 
-	const pipelineUrl = `${repository.links.html.href}/addon/pipelines/home#!/results/${build_number}`;
+    const pipelineUrl = `${repository.links.html.href}/addon/pipelines/home#!/results/${build_number}`;
 
-	return {
-		title: getTitle(),
-		subtitle: getSubtitle(),
-		arg: pipelineUrl
-	}
+    return {
+        title: getTitle(),
+        subtitle: getSubtitle(),
+        arg: pipelineUrl
+    }
 };
 
 module.exports = createService({ url, map });
--- pipelines.js
+++ pipelines.js
@@ -2,10 +2,10 @@ const alfy = require('alfy');
 const authenticate = require('./authenticate.js');
 
 authenticate().then(({ pipelineService }) => {
-	pipelineService.load({
-		query: alfy.input,
-		sort: '-created_on'
-	}).then(({ values })=> {
-		alfy.output(pipelineService.map(values));
-	});
+    pipelineService.load({
+        query: alfy.input,
+        sort: '-created_on'
+    }).then(({ values })=> {
+        alfy.output(pipelineService.map(values));
+    });
 });
