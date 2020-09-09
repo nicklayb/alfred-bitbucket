8845314df080739043ec4871293dfd5d4e677fbc
Sean Templeton <sean@templeton.io>
2020-09-09 12:52:01 -0500

    Refactor authenticate more and add error handling

--- authenticate.js
+++ authenticate.js
@@ -1,23 +1,38 @@
 const alfy = require('alfy');
-const bitbucket = require('./bitbucket/core').bitbucket;
+const {bitbucket} = require('./bitbucket/core');
 
 const ACCESS_TOKEN = 'access_token';
 const GRANT_TYPE = 'client_credentials';
 const ALIVE_TIME = 216000;
 const URL = 'https://bitbucket.org/site/oauth2/access_token';
 
+/**
+ * Generates a HTTP Basic Auth token for accessing the BitBucket API via App Passwords
+ *
+ * @param username {string}
+ * @param password {string}
+ * @returns {string}
+ */
 function getAppPasswordToken(username, password) {
     return `Basic ${Buffer.from([username, password].join(':')).toString('base64')}`;
 }
 
-async function getAuthToken(clientId, secret) {
+/**
+ * Generates a Bearer token for accessing the BitBucket API via OAuth2 Applications
+ *
+ * @param clientId
+ * @param secret
+ * @returns {Promise<string>}
+ */
+async function getOAuthToken(clientId, secret) {
     const accessToken = alfy.cache.get(ACCESS_TOKEN);
 
     if (accessToken) {
-        return bitbucket(accessToken);
+        return accessToken;
     }
 
     try {
+        // Request an access token from BitBucket using the OAuth2 flow
         const {access_token} = await alfy.fetch(URL, {
             auth: [clientId, secret].join(':'),
             json: true,
@@ -34,24 +49,38 @@ async function getAuthToken(clientId, secret) {
         return authToken;
     } catch (e) {
         alfy.cache.set(ACCESS_TOKEN, null);
+
+        throw e;
+    }
+}
+
+/**
+ * Attempts to get an authorization token for the BitBucket API either by App Passwords, or OAuth2 Applications
+ *
+ * @returns {Promise<string>}
+ */
+async function getAuthorizationToken() {
+    const {clientId, secret, appPassword, username} = process.env;
+
+    if (username && appPassword) {
+        return getAppPasswordToken(username, appPassword);
     }
 
-    return null;
+    if (clientId && secret) {
+        return await getOAuthToken(clientId, secret);
+    }
+
+    throw new Error('No OAuth2 Token or App Password set.');
 }
 
 async function loginAttempt() {
-    const {clientId, secret, appPassword, username} = process.env;
+    try {
+        return bitbucket(await getAuthorizationToken());
+    } catch (e) {
+        alfy.error('OAuth2 or App Password not set. Refer to README for details');
 
-    if ((!username || !appPassword) && (!clientId || !secret)) {
-        alfy.error('OAuth2 not set. Refer to README for details');
-        return null;
+        return bitbucket(null);
     }
-
-    if (username && appPassword) {
-        return bitbucket(getAppPasswordToken(username, appPassword));
-    }
-
-    return bitbucket(await getAuthToken(clientId, secret));
 }
 
 module.exports = loginAttempt;
